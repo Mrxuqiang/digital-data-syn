bfcc937f9e4219cdc474c448e6e4394a20182c05
xq0603 <tenderness_xq@163.com>
2016-05-17 10:56:05 +0800

    表结构修改

--- documents/digital/定时任务/task.sql
+++ documents/digital/定时任务/task.sql
@@ -16,4 +16,11 @@ shop_id,contract_id
 --更新oms_contract_brand_series
 brand_id
 
+--更新oms_market_info
+小区id second_org_id,大区名first_org_name,小区名second_org_name,lon,lat
 
+--更新oms_dealer
+dealer_type 判断经销商类型 province_id,city_id 需要从rem1.0里面读取 org_cretification_no没有找到
+
+--更新oms_contract
+contract_number  dealer_id导入我们这边的经销商id
